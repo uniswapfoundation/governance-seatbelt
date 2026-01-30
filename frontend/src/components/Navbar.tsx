82aec8debac9b5ca74e830541b213c128bc4fe24
marcomariscal <42938673+marcomariscal@users.noreply.github.com>
2026-01-30 10:08:53 -0800

    ui: show chain in ConnectButton

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,8 +2,6 @@
 
 import { useSimulationResults } from '@/hooks/use-simulation-results';
 import { parseSimulationType } from '@/lib/write-actions';
-import { Badge } from '@/components/ui/badge';
-import { appChain } from '@/config';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { CheckCircleIcon, FileTextIcon, PlayIcon, SendIcon } from 'lucide-react';
 import Link from 'next/link';
@@ -63,10 +61,7 @@ export function Navbar() {
           </div>
 
           <div className="flex items-center">
-            <Badge variant="outline" className="mr-2">
-              {appChain.name} ({appChain.id})
-            </Badge>
-            <ConnectButton chainStatus="name" />
+            <ConnectButton chainStatus="full" />
           </div>
         </div>
       </div>
--- frontend/src/config/wagmi.ts
+++ frontend/src/config/wagmi.ts
@@ -46,7 +46,6 @@ const getChain = (): { chain: Chain; rpcUrl: string } => {
 
 const { chain, rpcUrl } = getChain();
 
-export const appChain = chain;
 export const queryClient = new QueryClient();
 
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
