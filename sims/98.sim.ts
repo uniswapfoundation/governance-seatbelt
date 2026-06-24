28f2dff464d12cea05c9a67ce05a9aa69db21901
jtriley <jt.riley@uniswap.org>
2026-06-24 13:03:49 -0700

    udpate prop 98

--- sims/98.sim.ts
+++ sims/98.sim.ts
@@ -7,6 +7,11 @@ const OMNICHAIN_PROPOSAL_SENDER_ABI = parseAbi([
     "function setTrustedRemoteAddress(uint16 remoteChainId, bytes calldata remoteAddress)"
 ]);
 
+const PROXY_ADMIN_ABI = parseAbi([
+    "function transferOwnership(address)",
+    "function changeProxyAdmin(address proxy, address newAdmin)"
+]);
+
 const V2_FACTORY_ABI = parseAbi([
     "function setFeeToSetter(address)"
 ]);
@@ -21,7 +26,7 @@ const POOL_MANAGER_ABI = parseAbi([
 
 const OP_PORTAL2_ABI = parseAbi([
     "function depositTransaction(address _to, uint256 _value, uint64 _gasLimit, bool _isCreation, bytes memory _data)"
-])
+]);
 
 const ETHEREUM = {
     OMNICHAIN_PROPOSAL_SENDER: getAddress("0xeb0BCF27D1Fb4b25e708fBB815c421Aeb51eA9fc"),
@@ -29,11 +34,16 @@ const ETHEREUM = {
 
 const MEGA = {
     OMNICHAIN_GOVERNANCE_EXECUTOR: getAddress("0x8819b86ddF592c3aaAa6f9ec7cE1A0f99FC4322c"),
+    OMNICHAIN_GOVERNANCE_EXECUTOR_2: getAddress("0x51F9629C1e75aF07421E662DBEb2B7dc8deDefd9"),
     LZ_ENDPOINT: getAddress("0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7"),
     WORMHOLE_RECEIVER: getAddress("0xa107580F73BD797Bd8b87Ff24e98346D99F93DdB"),
     V2_FACTORY: getAddress("0xbf56488c857A881ae7e3BED27Cf99c10A7Ab7e50"),
     V3_FACTORY: getAddress("0x3a5F0CD7d62452b7f899B2A5758BFa57be0dE478"),
     POOL_MANAGER: getAddress("0xaCB7e78fa05D562e0A5D3089ec896D57D057d38E"),
+    V3_PROXY_ADMIN: getAddress("0xdaFBcEB5cA32Dc1DD27A413dA361F32636694BC4"),
+    V4_PROXY_ADMIN: getAddress("0x07e7c1cEd961e2C11196751A1aC76E64b0e8b007"),
+    V3_POSITION_DESCRIPTOR: getAddress("0x8D9F62d363486ebadD3F3d735301a99a487a8fD8"),
+    V4_POSITION_DESCRIPTOR: getAddress("0xA9fDbB9D3dce2e1cFB91c4AF1B8Cf4ed62c0041A"),
 };
 
 const AVAX = {
@@ -43,6 +53,10 @@ const AVAX = {
     V2_FACTORY: getAddress("0x9e5A52f57b3038F1B8EeE45F28b3C1967e22799C"),
     V3_FACTORY: getAddress("0x740b1c1de25031C31FF4fC9A62f554A55cdC1baD"),
     POOL_MANAGER: getAddress("0x06380C0e0912312B5150364B9DC4542BA0DbBc85"),
+    V3_PROXY_ADMIN: getAddress("0x9AdA7D7879214073F40183F3410F2b3f088c6381"),
+    V4_PROXY_ADMIN: getAddress("0x9b0481d2A2912051f56dC0B806cafe6bdE461c3D"),
+    V3_POSITION_DESCRIPTOR: getAddress("0xE1f93a7cB6fFa2dB4F9d5A2FD43158A428993C09"),
+    V4_POSITION_DESCRIPTOR: getAddress("0x2b1AED9445B05AC1A3B203eCCC1e25dD9351F0A9"),
 };
 
 const SONEIUM = {
@@ -63,9 +77,9 @@ const LZ_AVAX_CHAIN_ID = 106;
 const LZ_MEGA_CHAIN_ID = 398;
 
 // -------------------------------------------------------------------------------------------------
-// 01: Finalize Layer Zero Configuration for MegaEth
+// NN: (Ethereum) Set Layer Zero Trusted Remote For Mega
 //
-const ethSetTrustedRemoteToMega = {
+const ethSetTrustedRemoteToMega0x88 = {
     target: ETHEREUM.OMNICHAIN_PROPOSAL_SENDER,
     value: 0n,
     signature: "",
@@ -80,40 +94,50 @@ const ethSetTrustedRemoteToMega = {
 };
 
 // -------------------------------------------------------------------------------------------------
-// 02: Transfer Ownership on MegaETH from Layer Zero to Wormhole
+// NN: (Mega) Transfer Ownership on MegaETH from Layer Zero to Wormhole
 //
-const lzCallsOnMega = {
-    v2SetFeeToSetter: {
+const megaLzTransferOwnershipCalls = [
+    {
         target: MEGA.V2_FACTORY,
         value: 0n,
         signature: "",
-        data: encodeFunctionData({
+        calldata: encodeFunctionData({
             abi: V2_FACTORY_ABI,
             functionName: "setFeeToSetter",
             args: [MEGA.WORMHOLE_RECEIVER]
         })
     },
-    v3SetOwner: {
+    {
         target: MEGA.V3_FACTORY,
         value: 0n,
         signature: "",
-        data: encodeFunctionData({
+        calldata: encodeFunctionData({
             abi: V3_FACTORY_ABI,
             functionName: "setOwner",
             args: [MEGA.WORMHOLE_RECEIVER]
         })
     },
-    poolManagerTransferOwnership: {
+    {
         target: MEGA.POOL_MANAGER,
         value: 0n,
         signature: "",
-        data: encodeFunctionData({
+        calldata: encodeFunctionData({
             abi: POOL_MANAGER_ABI,
             functionName: "transferOwnership",
             args: [MEGA.WORMHOLE_RECEIVER]
         })
     },
-};
+    {
+        target: MEGA.V4_PROXY_ADMIN,
+        value: 0n,
+        signature: "",
+        calldata: encodeFunctionData({
+            abi: PROXY_ADMIN_ABI,
+            functionName: "transferOwnership",
+            args: [MEGA.WORMHOLE_RECEIVER]
+        })
+    },
+];
 
 const megaTransferOwnershipFromLzToWormhole = {
     target: ETHEREUM.OMNICHAIN_PROPOSAL_SENDER,
@@ -127,26 +151,10 @@ const megaTransferOwnershipFromLzToWormhole = {
             encodeAbiParameters(
                 parseAbiParameters("address[] targets, uint256[] values, string[] signatures, bytes[] datas"),
                 [
-                    [
-                        lzCallsOnMega.v2SetFeeToSetter.target,
-                        lzCallsOnMega.v3SetOwner.target,
-                        lzCallsOnMega.poolManagerTransferOwnership.target,
-                    ],
-                    [
-                        lzCallsOnMega.v2SetFeeToSetter.value,
-                        lzCallsOnMega.v3SetOwner.value,
-                        lzCallsOnMega.poolManagerTransferOwnership.value,
-                    ],
-                    [
-                        lzCallsOnMega.v2SetFeeToSetter.signature,
-                        lzCallsOnMega.v3SetOwner.signature,
-                        lzCallsOnMega.poolManagerTransferOwnership.signature,
-                    ],
-                    [
-                        lzCallsOnMega.v2SetFeeToSetter.data,
-                        lzCallsOnMega.v3SetOwner.data,
-                        lzCallsOnMega.poolManagerTransferOwnership.data,
-                    ],
+                    megaLzTransferOwnershipCalls.map(lzCall => lzCall.target),
+                    megaLzTransferOwnershipCalls.map(lzCall => lzCall.value),
+                    megaLzTransferOwnershipCalls.map(lzCall => lzCall.signature),
+                    megaLzTransferOwnershipCalls.map(lzCall => lzCall.calldata),
                 ]
             ),
             "0x"
@@ -155,42 +163,67 @@ const megaTransferOwnershipFromLzToWormhole = {
 };
 
 // -------------------------------------------------------------------------------------------------
-// 03: Transfer Ownership on Avax from Layer Zero to Wormhole
+// NN: Transfer Ownership on Avax from Layer Zero to Wormhole
 //
-const lzCallsOnAvax = {
-    v2SetFeeToSetter: {
+const avaxLzTransferOwnershipCalls = [
+        {
         target: AVAX.V2_FACTORY,
         value: 0n,
         signature: "",
-        data: encodeFunctionData({
+        calldata: encodeFunctionData({
             abi: V2_FACTORY_ABI,
             functionName: "setFeeToSetter",
             args: [AVAX.WORMHOLE_RECEIVER]
         })
     },
-    v3SetOwner: {
+    {
         target: AVAX.V3_FACTORY,
         value: 0n,
         signature: "",
-        data: encodeFunctionData({
+        calldata: encodeFunctionData({
             abi: V3_FACTORY_ABI,
             functionName: "setOwner",
             args: [AVAX.WORMHOLE_RECEIVER]
         })
     },
-    poolManagerTransferOwnership: {
+    {
         target: AVAX.POOL_MANAGER,
         value: 0n,
         signature: "",
-        data: encodeFunctionData({
+        calldata: encodeFunctionData({
             abi: POOL_MANAGER_ABI,
             functionName: "transferOwnership",
             args: [AVAX.WORMHOLE_RECEIVER]
         })
     },
-};
+    {
+        target: AVAX.V3_PROXY_ADMIN,
+        value: 0n,
+        signature: "",
+        calldata: encodeFunctionData({
+            abi: PROXY_ADMIN_ABI,
+            functionName: "changeProxyAdmin",
+            args: [
+                AVAX.V3_POSITION_DESCRIPTOR,
+                AVAX.V4_PROXY_ADMIN,
+            ]
+        })
+    },
+    {
+        target: AVAX.V4_PROXY_ADMIN,
+        value: 0n,
+        signature: "",
+        calldata: encodeFunctionData({
+            abi: PROXY_ADMIN_ABI,
+            functionName: "transferOwnership",
+            args: [
+                AVAX.WORMHOLE_RECEIVER
+            ]
+        })
+    }
+];
 
-const avaxTransferOwnershipFromLzToWormhole = {
+const avaxTransferOwnershipFromLztoWormhole = {
     target: ETHEREUM.OMNICHAIN_PROPOSAL_SENDER,
     value: 0n,
     signature: "",
@@ -202,26 +235,10 @@ const avaxTransferOwnershipFromLzToWormhole = {
             encodeAbiParameters(
                 parseAbiParameters("address[] targets, uint256[] values, string[] signatures, bytes[] datas"),
                 [
-                    [
-                        lzCallsOnAvax.v2SetFeeToSetter.target,
-                        lzCallsOnAvax.v3SetOwner.target,
-                        lzCallsOnAvax.poolManagerTransferOwnership.target,
-                    ],
-                    [
-                        lzCallsOnAvax.v2SetFeeToSetter.value,
-                        lzCallsOnAvax.v3SetOwner.value,
-                        lzCallsOnAvax.poolManagerTransferOwnership.value,
-                    ],
-                    [
-                        lzCallsOnAvax.v2SetFeeToSetter.signature,
-                        lzCallsOnAvax.v3SetOwner.signature,
-                        lzCallsOnAvax.poolManagerTransferOwnership.signature,
-                    ],
-                    [
-                        lzCallsOnAvax.v2SetFeeToSetter.data,
-                        lzCallsOnAvax.v3SetOwner.data,
-                        lzCallsOnAvax.poolManagerTransferOwnership.data,
-                    ],
+                    avaxLzTransferOwnershipCalls.map(lzCall => lzCall.target),
+                    avaxLzTransferOwnershipCalls.map(lzCall => lzCall.value),
+                    avaxLzTransferOwnershipCalls.map(lzCall => lzCall.signature),
+                    avaxLzTransferOwnershipCalls.map(lzCall => lzCall.calldata),
                 ]
             ),
             "0x"
@@ -229,9 +246,8 @@ const avaxTransferOwnershipFromLzToWormhole = {
     })
 };
 
-
 // -------------------------------------------------------------------------------------------------
-// 04: Transfer V2 Fee Setter on Soneium from OptimismPortal2 to CrossChainAccount
+// NN: Transfer V2 Fee Setter on Soneium from OptimismPortal2 to CrossChainAccount
 //
 const soneiumV2SetFeeToSetter = {
     target: SONEIUM.OPTIMISM_PORTAL2,
@@ -255,7 +271,7 @@ const soneiumV2SetFeeToSetter = {
 };
 
 // -------------------------------------------------------------------------------------------------
-// 05: Transfer V4 Ownership on Soneium from OptimismPortal2 to CrossChainAccount
+// NN: Transfer V4 Ownership on Soneium from OptimismPortal2 to CrossChainAccount
 //
 const soneiumPoolManagerTransferOwnership = {
     target: SONEIUM.OPTIMISM_PORTAL2,
@@ -303,7 +319,7 @@ const xLayerV2SetFeeToSetter = {
 };
 
 // -------------------------------------------------------------------------------------------------
-// 07: Transfer V4 Ownership on XLayer from OptimismPortal2 to CrossChainAccount
+// NN: Transfer V4 Ownership on XLayer from OptimismPortal2 to CrossChainAccount
 //
 const xLayerPoolManagerTransferOwnership = {
     target: X_LAYER.OPTIMISM_PORTAL2,
@@ -326,47 +342,79 @@ const xLayerPoolManagerTransferOwnership = {
     })
 };
 
+// -------------------------------------------------------------------------------------------------
+// NN: (Ethereum) Set Layer Zero Trusted Remote For Mega (OPTIONAL)
+//
+const ethSetTrustedRemoteToMega0x55 = {
+    target: ETHEREUM.OMNICHAIN_PROPOSAL_SENDER,
+    value: 0n,
+    signature: "",
+    calldata: encodeFunctionData({
+        abi: OMNICHAIN_PROPOSAL_SENDER_ABI,
+        functionName: "setTrustedRemoteAddress",
+        args: [
+            LZ_MEGA_CHAIN_ID,
+            encodePacked(["address"], [MEGA.OMNICHAIN_GOVERNANCE_EXECUTOR_2])
+        ]
+    })
+};
+
+// -------------------------------------------------------------------------------------------------
+// NN: (Mega) Change ProxyAdmin for NonfungiblePositionDescriptor (OPTIONAL)
+//
+const megaChangeProxyAdmin = {
+    target: ETHEREUM.OMNICHAIN_PROPOSAL_SENDER,
+    value: 0n,
+    signature: "",
+    calldata: encodeFunctionData({
+        abi: OMNICHAIN_PROPOSAL_SENDER_ABI,
+        functionName: "execute",
+        args: [
+            LZ_MEGA_CHAIN_ID,
+            encodeAbiParameters(
+                parseAbiParameters("address[] targets, uint256[] values, string[] signatures, bytes[] datas"),
+                [
+                    [MEGA.V3_PROXY_ADMIN],
+                    [0n],
+                    [""],
+                    [
+                        encodeFunctionData({
+                            abi: PROXY_ADMIN_ABI,
+                            functionName: "changeProxyAdmin",
+                            args: [
+                                MEGA.V3_POSITION_DESCRIPTOR,
+                                MEGA.V4_PROXY_ADMIN
+                            ]
+                        })
+                    ],
+                ]
+            ),
+            "0x"
+        ]
+    })
+};
+
+const actions = [
+    ethSetTrustedRemoteToMega0x88,
+    megaTransferOwnershipFromLzToWormhole,
+    avaxTransferOwnershipFromLztoWormhole,
+    soneiumV2SetFeeToSetter,
+    soneiumPoolManagerTransferOwnership,
+    xLayerV2SetFeeToSetter,
+    xLayerPoolManagerTransferOwnership,
+    ethSetTrustedRemoteToMega0x55,
+    megaChangeProxyAdmin,
+];
+
 export const config: SimulationConfigNew = {
     type: "new",
     daoName: "Uniswap",
     governorAddress: getAddress("0x408ED6354d4973f66138C91495F2f2FCbd8724C3"),
     governorType: "bravo",
-    targets: [
-        ethSetTrustedRemoteToMega.target,
-        megaTransferOwnershipFromLzToWormhole.target,
-        avaxTransferOwnershipFromLzToWormhole.target,
-        soneiumV2SetFeeToSetter.target,
-        soneiumPoolManagerTransferOwnership.target,
-        xLayerV2SetFeeToSetter.target,
-        xLayerPoolManagerTransferOwnership.target
-    ],
-    values: [
-        ethSetTrustedRemoteToMega.value,
-        megaTransferOwnershipFromLzToWormhole.value,
-        avaxTransferOwnershipFromLzToWormhole.value,
-        soneiumV2SetFeeToSetter.value,
-        soneiumPoolManagerTransferOwnership.value,
-        xLayerV2SetFeeToSetter.value,
-        xLayerPoolManagerTransferOwnership.value
-    ],
-    signatures: [
-        ethSetTrustedRemoteToMega.signature,
-        megaTransferOwnershipFromLzToWormhole.signature,
-        avaxTransferOwnershipFromLzToWormhole.signature,
-        soneiumV2SetFeeToSetter.signature,
-        soneiumPoolManagerTransferOwnership.signature,
-        xLayerV2SetFeeToSetter.signature,
-        xLayerPoolManagerTransferOwnership.signature
-    ],
-    calldatas: [
-        ethSetTrustedRemoteToMega.calldata,
-        megaTransferOwnershipFromLzToWormhole.calldata,
-        avaxTransferOwnershipFromLzToWormhole.calldata,
-        soneiumV2SetFeeToSetter.calldata,
-        soneiumPoolManagerTransferOwnership.calldata,
-        xLayerV2SetFeeToSetter.calldata,
-        xLayerPoolManagerTransferOwnership.calldata
-    ],
+    targets: actions.map(action => action.target),
+    values: actions.map(action => action.value),
+    signatures: actions.map(action => action.signature),
+    calldatas: actions.map(action => action.calldata),
     description: `
 # [RFC] Update Crosschain Governance Parameters for Avalanche, MegaETH, Soneium, and X Layer
 
